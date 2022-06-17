a34967b8aef67bac88cb8c9de13a38d00835a5ac
yasinkiziltas <kzltasyasin@gmail.com>
2022-06-17 16:24:25 +0300

    .

--- src/screens/auth/Register.js
+++ src/screens/auth/Register.js
@@ -6,46 +6,8 @@ import back from '../../img/back.png'
 
 export default function Register() {
   return (
-    <div className='content'>
-      <Container fluid>
-        <Row>
-          <Col lg={6} md={6} xs={12}>
-            <div className='welcomeContainer'>
-              <h2>CORL CORP.</h2>
-              <p className='subText'>Tekrar Hoşgeldiniz! Lütfen kayıt olunuz.</p>
-
-              <div style={{ justifyContent: 'center', alignItems: 'center', display: 'grid' }}>
-                <Input
-                  name="username"
-                  style={{ marginBottom: 15 }}
-                  placeholder="Kullanıcı Adı"
-                />
-
-                <Input
-                  name="username"
-                  placeholder="Şifre"
-                />
-
-                <div style={{ alignItems: 'center' }} className="d-flex flex-column">
-                  <Button
-                    style={{ width: 320, marginBottom: 15 }}
-                    color="success"
-                    outline
-                  >Kayıt Ol</Button>
-
-                  
-                </div>
-              </div>
-            </div>
-          </Col>
-
-          <Col lg={6} md={6} xs={12} className='imageContainer'>
-            <div>
-              <img className='img' src={back} />
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+      <div>
+        
+      </div>
   )
 }
